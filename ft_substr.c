1646fd2eeebb7aaeb6ef65201552834decb43368
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-06 22:51:15 +0100

    Complete ft_substr.c function implementation.

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 23:13:36 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/06 20:37:34 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/06 22:17:46 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	const char		*save;
 	unsigned int	i;
 
-	i = 0;
-	while (i++ < start)
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	while (start--)
 		s++;
 	i = 0;
 	save = s;
-	while (*s && i++ < len)
+	while (*s && i < len)
+	{
 		s++;
-	str = (char *)malloc(sizeof(*s) * (i + 1));
+		i++;
+	}
+	if (!(str = (char *)malloc(sizeof(*s) * (i + 1))))
+		return (NULL);
 	ft_strlcpy(str, save, i + 1);
 	return (str);
 }
