fe9f2be30628d84400e7432a0de7b9aa0a3fd346
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-06 12:08:26 +0100

    Complete ft_strdup.c function implementation.

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,25 +6,32 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:59:12 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/03 22:59:14 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/06 11:51:51 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_strcpy(char *dst, const char *src)
+{
+	char	*buf;
+
+	buf = dst;
+	while (*src)
+		*buf++ = *src++;
+	*buf = '\0';
+	return (dst);
+}
+
 char	*ft_strdup(const char *src)
 {
-	int		i;
-	char	*output;
+	char	*str;
 
-	i = 0;
-	while (src[i])
-		i++;
-	if (!(output = malloc(sizeof(char) * (i + 1))))
+	if (!(str = malloc(sizeof(*src) * (ft_strlen(src) + 1))))
 	{
 		errno = ENOMEM;
 		return (NULL);
 	}
-	ft_strcpy(output, src);
-	return (output);
+	ft_strcpy(str, src);
+	return (str);
 }
