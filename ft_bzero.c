08132733177b6b95b9488b934973b5f391917da4
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-11 23:40:05 +0100

    Complete ft_bzero.c function implementation.

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:56:52 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/11 23:36:10 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/11 23:39:27 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	while (n--)
-		*(unsigned char *)s++ = '\0';
+	ft_memset(s, 0, n);
 }
