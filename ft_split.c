2f98e89ad779a293018983160527064b918a5940
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-11 21:13:50 +0100

    Complete ft_split.c function implementation.

--- ft_split.c
+++ ft_split.c
@@ -6,25 +6,11 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 23:22:32 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/10 23:58:28 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/11 21:12:02 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
-
-int	ft_wordlen(char const *word, char c)
-{
-	int	length;
-
-	length = 0;
-	while (*word && *word != c)
-	{
-		word++;
-		length++;
-	}
-	return (length);
-}
 
 int	ft_wordcount(char const *s, char c)
 {
@@ -35,11 +21,10 @@ int	ft_wordcount(char const *s, char c)
 	{
 		while (*s == c)
 			s++;
-		s = ft_strchr(s, c);
-		if (s)
+		if (*s)
 			wc++;
+		s = ft_strchr(s, c);
 	}
-	printf("%d\n", wc);
 	return (wc);
 }
 
@@ -47,29 +32,30 @@ char	**ft_split(char const *s, char c)
 {
 	char	**strs;
 	char	**buf;
-	//int		wl;
 	char	*next;
 
 	strs = (char **)malloc((ft_wordcount(s, c) + 1) * sizeof(*strs));
 	if (!strs)
 		return (NULL);
 	buf = strs;
-	while (s && *s)
+	while (*s)
 	{
 		while (*s == c)
 			s++;
-		next = ft_strchr(s, c);
-		if (!next)
+		if (*s)
 		{
-			next = (char *)s;
-			while (*next)
-				next++;
+			next = ft_strchr(s, c);
+			if (!next || !c)
+			{
+				next = (char *)s;
+				while (*next)
+					next++;
+			}
+			*strs = ft_substr(s, 0, next - s);
+			strs++;
+			s = next;
 		}
-		*strs = ft_substr(s, 0, (next - s)); 
-		s = next;
-		strs++;
 	}
-	//strs--;
 	*strs = 0;
 	return (buf);
 }
