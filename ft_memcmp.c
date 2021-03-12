2e913bc759d98c241895a54a45b802c692ab3cca
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-12 22:28:15 +0100

    Complete ft_memcmp.c function implementation.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,24 +6,17 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:58:16 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/03 22:58:18 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/12 22:27:39 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *str1, const void *str2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*s1;
-	unsigned char	*s2;
-
-	i = 0;
-	s1 = (unsigned char *)str1;
-	s2 = (unsigned char *)str2;
-	if (n == 0)
+	if (!n)
 		return (0);
-	while (s1[i] == s2[i] && i < n - 1)
-		i++;
-	return (s1[i] - s2[i]);
+	while (*(unsigned char *)s1++ == *(unsigned char *)s2++ && --n)
+		;
+	return (*(unsigned char *)(--s1) - *(unsigned char *)(--s2));
 }
