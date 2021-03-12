0dec121c4d376f06369ba88a5c61fae2607e845d
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-12 22:34:58 +0100

    Complete ft_strlcpy.c function implementation.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,20 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 12:48:35 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/06 20:46:35 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/12 22:34:40 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t 		i;
 	const char	*buf;
 
 	buf = src;
 	if (src && dst && size)
 	{
-		i = 0;
-		while (*src && i++ < size - 1)
+		while (*src && --size)
 			*dst++ = *src++;
 		*dst = 0;
 	}
