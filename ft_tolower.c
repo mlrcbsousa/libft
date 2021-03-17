256e63bbd015b47f3cb7c740ffbc9b676e44c5ef
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-17 20:42:16 +0100

    Complete ft_tolower.c function implementation.

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,15 +6,13 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 23:00:02 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/03 23:00:11 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/17 20:41:54 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 int	ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		c = c + 32;
+		c += 32;
 	return (c);
 }
