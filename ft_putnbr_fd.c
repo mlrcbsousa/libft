8a9e9569da6e5aad22154300dc68d7bfde306078
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-17 23:24:29 +0100

    Complete ft_putnbr_fd.c function implementation.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:58:51 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/03 22:58:55 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/17 23:24:06 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 void	ft_putnbr_fd(int nb, int fd)
 {
 	if (nb == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
 	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		nb = 147483648;
+		if (nb < 0)
+		{
+			ft_putchar_fd('-', fd);
+			nb *= -1;
+		}
+		if (nb > 9)
+			ft_putnbr_fd(nb / 10, fd);
+		ft_putchar_fd(nb % 10 + '0', fd);
 	}
-	if (nb < 0)
-	{
-		ft_putchar_fd('-', fd);
-		nb *= -1;
-	}
-	if (nb >= 10)
-		ft_putnbr_fd(nb / 10, fd);
-	ft_putchar_fd(nb % 10 + '0', fd);
 }
