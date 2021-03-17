14196377f98ffa3c9a81c712e2d92e2b4ba881b8
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-17 20:19:00 +0100

    Complete ft_atoi.c function implementation.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,45 +6,34 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:53:31 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/03 22:53:34 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/17 20:18:29 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_return(int c, unsigned long long ret, int neg)
+static int	ft_isspace(char c)
 {
-	if (c >= 19)
-		return (neg == -1 ? 0 : -1);
-	if (ret > 9223372036854775807)
-		return (neg == 1 ? 0 : -1);
-	else
-		return (ret * neg);
+	if (c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v' || c == ' ')
+		return (1);
+	return (0);
 }
 
-int			ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	unsigned long long	negative;
-	int					i;
-	int					count;
-	unsigned long long	number;
+	int			sign;
+	long long	number;
 
-	negative = 1;
-	i = 0;
+	sign = 1;
 	number = 0;
-	count = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] == '\v'
-			|| str[i] == '\f' || str[i] == ' ')
-		i++;
-	if (str[i] == '-')
-		negative = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while ((str[i] >= '0' && str[i] <= '9') && str[i])
-	{
-		number = (number * 10) + str[i] - '0';
-		count++;
-		i++;
-	}
-	return (ft_return(count, number, negative));
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (ft_isdigit(*str))
+		number = (number * 10) + *str++ - '0';
+	return ((int)(number * sign));
 }
