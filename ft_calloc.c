b44c9b2528afc094d9ea2853562d1666d2c8afcd
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-17 20:46:22 +0100

    Complete ft_calloc.c function implementation.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,20 +6,20 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 23:15:14 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/04 00:20:57 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/17 20:45:43 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void *ret;
-	
+	void	*ret;
+
 	if (INT_MAX / size < nmemb)
 		return (NULL);
 	ret = malloc(nmemb * size);
-    if (ret)  
-        ft_memset(ret, 0, nmemb * size);
-    return ret;
+	if (ret)
+		ft_memset(ret, 0, nmemb * size);
+	return (ret);
 }
