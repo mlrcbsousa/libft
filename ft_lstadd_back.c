18845ba11ae74cc42064f058ae8b6a1f9e41f7ee
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-18 22:29:38 +0100

    Complete ft_lstadd_back.c function implementation.

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,28 +6,16 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 23:47:15 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/18 00:55:13 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/18 22:28:13 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_list_last(t_list *lst)
-{
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
-}
-
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*last;
-
 	if (*lst)
-	{
-		last = ft_list_last(*lst);
-		last->next = new;
-	}
+		ft_lstlast(*lst)->next = new;
 	else
 		*lst = new;
 }
