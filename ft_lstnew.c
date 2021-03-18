bc33bd856523bbc43e4b1cb2f152f78be640477c
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-18 22:07:37 +0100

    Complete ft_lstnew.c function implementation.

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 23:59:56 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/17 23:59:58 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/18 21:51:10 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*elem;
 
 	elem = (t_list *)malloc(sizeof(t_list));
+	if (!elem)
+		return (NULL);
 	elem->content = content;
 	elem->next = NULL;
 	return (elem);
