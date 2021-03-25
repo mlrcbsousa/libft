430f86107f2dba530f60838f22a5cf4f45ccc072
Manuel Sousa <mlrcbsousa@gmail.com>
2021-03-25 19:18:38 +0100

    Fix bug with strchr.

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: manuel <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:55:27 by manuel            #+#    #+#             */
-/*   Updated: 2021/03/16 23:01:38 by manuel           ###   ########.fr       */
+/*   Updated: 2021/03/25 19:18:22 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *str, int c)
 {
+	if (!str)
+		return (NULL);
 	while (*str)
 		if (*str++ == (char)c)
 			return ((char *)(str - 1));
