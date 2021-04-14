0e45be8740c8d8eb4556eba0f6677059f6d40a30
Manuel Sousa <msousa@c1r9s1.42lisboa.com>
2021-04-14 18:40:58 +0100

    Remove max check on calloc.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 22:49:01 by msousa            #+#    #+#             */
-/*   Updated: 2021/04/12 22:49:26 by msousa           ###   ########.fr       */
+/*   Updated: 2021/04/14 18:40:43 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ret;
 
-	if (INT_MAX / size < nmemb)
-		return (NULL);
 	ret = malloc(nmemb * size);
 	if (ret)
 		ft_bzero(ret, nmemb * size);
